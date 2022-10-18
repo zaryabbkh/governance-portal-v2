3edb28e01aa4a4eae5c66b26fc6f12c5dde013e7
zaryab <zaryabbkh@gmail.com>
2022-10-18 13:38:57 +0500

    spock urls added for graphql

--- modules/gql/gql.constants.ts
+++ modules/gql/gql.constants.ts
@@ -1,10 +1,11 @@
 import { config } from "lib/config";
 
 /* Spock URLs */
-export const LOCAL_SPOCK_URL = config.LOCAL_SPOCK_URL;
-export const GOERLI_SPOCK_URL = config.GOERLI_SPOCK_URL;
-export const STAGING_MAINNET_SPOCK_URL = config.STAGING_MAINNET_SPOCK_URL;
-export const MAINNET_SPOCK_URL = config.MAINNET_SPOCK_URL;
+
+export const LOCAL_SPOCK_URL = "http://localhost:3001/v1"
+export const GOERLI_SPOCK_URL = "https://polling-db-goerli.gsuprotocol.io/v1"
+export const STAGING_MAINNET_SPOCK_URL = "https://polling-db-staging.gsuprotocol.io/v1"
+export const MAINNET_SPOCK_URL = "https://polling-db-prod.gsuprotocol.io/v1"
 
 export enum QueryFilterNames {
   Active = 'active',
