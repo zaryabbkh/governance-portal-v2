a58fda491760e5c92b4e75a8ead06db8bf51c488
zaryab <zaryabbkh@gmail.com>
2022-11-05 17:00:35 +0500

    beta release addresses added

--- modules/gql/gql.constants.ts
+++ modules/gql/gql.constants.ts
@@ -3,7 +3,7 @@ import { config } from "lib/config";
 /* Spock URLs */
 
 export const LOCAL_SPOCK_URL = "http://localhost:3001/v1"
-export const GOERLI_SPOCK_URL = "https://polling-db-goerli.gsuprotocol.io/v1"
+export const GOERLI_SPOCK_URL = "https://polling-db-goerli-beta.gsuprotocol.io/v1"
 export const STAGING_MAINNET_SPOCK_URL = "https://polling-db-staging.gsuprotocol.io/v1"
 export const MAINNET_SPOCK_URL = "https://polling-db-prod.gsuprotocol.io/v1"
 
